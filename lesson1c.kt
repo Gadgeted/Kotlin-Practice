f1994a23f4f3893e10bd53eec90968c066c754a0
Collins Ngunyi <129302972+Gadgeted@users.noreply.github.com>
2025-02-16 23:56:52 +0300

    Update lesson1c.kt

--- lesson1c.kt
+++ lesson1c.kt
@@ -1,9 +1,9 @@
 fun main() { 
     //Program to add 3 numbers
     //Arithmetic Operation
-    val num1: Int = 50
-    val num2:Int = 70
-    val num3: Int = 50
+    val num1: Int = 40
+    val num2:Int = 80
+    val num3: Int = 60
 
     val addition: Int = num1 + num2 +num3
 
