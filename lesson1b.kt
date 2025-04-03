a5175301964a531b506031191432a2708160cabb
Collins Ngunyi <129302972+Gadgeted@users.noreply.github.com>
2025-04-03 02:26:01 -1100

    Update lesson1b.kt

--- lesson1b.kt
+++ lesson1b.kt
@@ -1,7 +1,7 @@
 // Here we specify the data types
 
 fun main() {
-    val myNum: Int = 10    //Int
+    val myNum: Int = 11    //Int
     val myDoubleNum: Double = 5.99   //Double
     val myLetter: Char = 'B'  //Character
     val myBoolean: Boolean = true  //Boolean
